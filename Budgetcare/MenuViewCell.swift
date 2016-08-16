fc9fad911783f5abb5c2659ddf846370f6a8f993
calyr <calyr_software@hotmail.com>
2016-08-15 21:03:53 -0400

    init

---
+++ Budgetcare/MenuViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MenuViewCell.swift
+//  Budgetcare
+//
+//  Created by calyr on 8/15/16.
+//  Copyright © 2016 calyr. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
